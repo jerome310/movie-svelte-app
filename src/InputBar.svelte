60330b1a10917a76399ebb240c7f58627374664f
jerome310 <jeromeyoung333@gmail.com>
2023-06-17 13:27:44 -0400

    desktop - complete 80% of the figma design

--- src/InputBar.svelte
+++ src/InputBar.svelte
@@ -1,1 +1,14 @@
-<h1>Input Bar</h1>
+<script>
+
+</script>
+
+<div class="input-container">
+    <p>Email Address</p>
+    <input type="text" placeholder="email@company.com">
+</div>
+
+<style>
+    .input-container {
+        margin-top: 40px;
+    }
+</style>
